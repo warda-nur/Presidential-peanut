8d588c239a2c9b98d9f6eeef73cd6d4857ee76e5
Warda Nur <wardanur04@gmail.com>
2021-11-13 13:06:15 +0800

    updated UI design

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import Gallery from "./Gallery";
 import "./gallery.css";
+// import Card from "@mui/material/Card";
 
 function App() {
   return (
     <>
-      {/* <h1 style={{ textAlign: "center" }}>Presidential Peanut Gallery</h1> */}
-      <div style={{ textAlign: "center" }} className="wrapper">
-        <div className="typing-demo">Presidential Peanut Gallery.</div>
+      <div className="card">
+        {/* <h1 style={{ textAlign: "center" }}>Presidential Peanut Gallery</h1> */}
+        <div style={{ textAlign: "center" }} className="wrapper">
+          <div className="typing-demo">
+            Presidential Peanut <span>Gallery.</span>
+          </div>
+        </div>
+        <Gallery />
       </div>
-      <Gallery />
     </>
   );
 }
